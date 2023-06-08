3bd093b891a1824be25d23e4264bb6487656219f
TobiborRahman <r.tobibor16@gmail.com>
2023-06-08 12:19:56 +0600

    roles added

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+var jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -33,11 +34,34 @@ async function run() {
 			.db('musicalMagic')
 			.collection('addedClasses');
 
+		app.post('/jwt', async (req, res) => {
+			const user = req.body;
+			const token = jwt.sign(user, process.env.SECRET_TOKEN_PASS, {
+				expiresIn: '2 days',
+			});
+			res.send({ token });
+		});
+
+		// role changes apis
+		app.patch('/users/role/:id', async (req, res) => {
+			const id = req.params.id;
+			const { role } = req.body;
+			const filter = { _id: new ObjectId(id) };
+			const updateDoc = {
+				$set: {
+					role: role,
+				},
+			};
+			const result = await userCollection.updateOne(filter, updateDoc);
+			res.send(result);
+		});
+
 		// Users related apis
 		app.get('/users', async (req, res) => {
 			const result = await userCollection.find().toArray();
 			res.send(result);
 		});
+
 		app.post('/users', async (req, res) => {
 			const user = req.body;
 			const query = { email: user.email };
