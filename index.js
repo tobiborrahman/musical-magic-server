e91cb32f9c281a3814babcf1ef57144aedb68803
TobiborRahman <r.tobibor16@gmail.com>
2023-06-10 21:37:08 +0600

    jwt not working have to fix

--- index.js
+++ index.js
@@ -11,20 +11,41 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
+// const verifyJWT = (req, res, next) => {
+// 	const authorization = req.headers.authorization;
+
+// 	if (!authorization) {
+// 		return res.status(401).send({ error: 'Unauthorized' });
+// 	}
+
+// 	try {
+// 		const decoded = jwt.verify(token, process.env.SECRET_TOKEN_PASS);
+// 		req.decoded = decoded;
+// 		next();
+// 	} catch (error) {
+// 		return res.status(401).json({ error: 'Invalid token' });
+// 	}
+// };
+
 const verifyJWT = (req, res, next) => {
 	const authorization = req.headers.authorization;
-
 	if (!authorization) {
-		return res.status(401).send({ error: 'Unauthorized' });
+		return res
+			.status(401)
+			.send({ error: true, message: 'unauthorized access' });
 	}
+	// bearer token
+	const token = authorization.split(' ')[1];
 
-	try {
-		const decoded = jwt.verify(token, process.env.SECRET_TOKEN_PASS);
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+		if (err) {
+			return res
+				.status(401)
+				.send({ error: true, message: 'unauthorized access' });
+		}
 		req.decoded = decoded;
 		next();
-	} catch (error) {
-		return res.status(401).json({ error: 'Invalid token' });
-	}
+	});
 };
 
 // musicalMagic
@@ -49,6 +70,9 @@ async function run() {
 
 		const userCollection = client.db('musicalMagic').collection('users');
 		const classCollection = client.db('musicalMagic').collection('class');
+		const instructorCollection = client
+			.db('musicalMagic')
+			.collection('instructors');
 		const paymentCollection = client
 			.db('musicalMagic')
 			.collection('payments');
@@ -62,45 +86,6 @@ async function run() {
 				expiresIn: '7d',
 			});
 			res.send({ token });
-		});
-
-		// role changes api
-		// app.get('/users/role/:email', async (req, res) => {
-		// 	const email = req.params.email;
-
-		// 	try {
-		// 		const user = await userCollection.findOne({ email: email });
-
-		// 		if (!user) {
-		// 			return res.status(404).json({ error: 'User not found' });
-		// 		}
-
-		// 		const roles = user.roles || [];
-
-		// 		const result = {
-		// 			isAdmin: roles.includes('admin'),
-		// 			isInstructor: roles.includes('instructor'),
-		// 			isUser: roles.includes('user'),
-		// 		};
-
-		// 		res.send(result);
-		// 	} catch (error) {
-		// 		console.error('Error retrieving user roles:', error);
-		// 		res.status(500).json({ error: 'Internal server error' });
-		// 	}
-		// });
-
-		app.patch('/users/role/:id', async (req, res) => {
-			const id = req.params.id;
-			const { role } = req.body;
-			const filter = { _id: new ObjectId(id) };
-			const updateDoc = {
-				$set: {
-					role: role,
-				},
-			};
-			const result = await userCollection.updateOne(filter, updateDoc);
-			res.send(result);
 		});
 
 		// Users related apis
@@ -120,31 +105,57 @@ async function run() {
 			res.send(result);
 		});
 
-		// Admin related api
+		app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+			const email = req.params.email;
+
+			if (req.decoded.email !== email) {
+				res.send({ admin: false });
+			}
+
+			const query = { email: email };
+			const user = await userCollection.findOne(query);
+			const result = { admin: user?.role === 'admin' };
+			res.send(result);
+		});
+
+		app.patch('/users/admin/:id', async (req, res) => {
+			const id = req.params.id;
+			const filter = { _id: new ObjectId(id) };
+			const updateDoc = {
+				$set: {
+					role: 'admin',
+				},
+			};
+			const result = await userCollection.updateOne(filter, updateDoc);
+			res.send(result);
+		});
+
+		app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
+			const email = req.params.email;
+
+			if (req.decoded.email !== email) {
+				res.send({ instructor: false });
+			}
+
+			const query = { email: email };
+			const user = await userCollection.findOne(query);
+			const result = { instructor: user?.role === 'instructor' };
+			res.send(result);
+		});
+
+		app.patch('/users/instructor/:id', async (req, res) => {
+			const id = req.params.id;
+			const filter = { _id: new ObjectId(id) };
+			const updateDoc = {
+				$set: {
+					role: 'instructor',
+				},
+			};
+			const result = await userCollection.updateOne(filter, updateDoc);
+			res.send(result);
+		});
 
 		// selected classes by students
-		// app.get('/class/:email', verifyJWT, async (req, res) => {
-		// 	const email = req.query.email;
-		// 	if (!email) {
-		// 		return res.send([]);
-		// 	}
-
-		// 	const decodedEmail = req.decoded.email;
-
-		// 	console.log(decodedEmail);
-
-		// 	if (email !== decodedEmail) {
-		// 		return res.status(403).send({
-		// 			error: true,
-		// 			message: 'forbidden access',
-		// 		});
-		// 	}
-
-		// 	const query = { email: email };
-
-		// 	const result = await classCollection.find(query).toArray();
-		// 	res.send(result);
-		// });
 
 		app.get('/class', async (req, res) => {
 			const result = await classCollection.find().toArray();
@@ -169,10 +180,17 @@ async function run() {
 			res.send(result);
 		});
 
+		// Instructor apis
+		app.post('/instructors', async (req, res) => {
+			const query = req.body;
+			const result = await instructorCollection.insertOne(query);
+			res.send(result);
+		});
+
 		// Payment method api
 		app.post('/payment-intent-method', async (req, res) => {
 			const { price } = req.body;
-			const amount = price * 100;
+			const amount = parseInt(price * 100);
 			const paymentIntent = await stripe.paymentIntents.create({
 				amount: amount,
 				currency: 'usd',
